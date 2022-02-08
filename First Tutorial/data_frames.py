36ef8bbc25c75ae527ab84acc26ad19126161bd6
bence <vas.bence01@gmail.com>
2022-02-08 21:01:20 +0100

    Következő alkalomra marad a pandas missing data

--- First Tutorial/data_frames.py
+++ First Tutorial/data_frames.py
@@ -30,6 +30,35 @@ def main():
     print(my_data_frame.iloc[0])
     #Determine number of rows and columns
     print(f"Rows and columns: {my_data_frame.shape}")
+    print("")
+    #Slicing Data frames
+    print(my_data_frame[["A","B"]].loc[["X","Y"]])
+    print("")
+    #Items bigger than element
+    print(my_data_frame > 4)
+    print("")
+    #If the values not bigger than number then NaN will it be the value
+    print(my_data_frame[my_data_frame > 0.5])
+    print("")
+    #The same thing for columns
+    print(my_data_frame["C"]<1)
+    print("")
+    #The same thing for rows
+    print(my_data_frame[my_data_frame["C"]<1])
+    print("")
+    #We can use both at the same time
+    print(my_data_frame[(my_data_frame['C'] > 0) & (my_data_frame['A']> 0)])
+    print("")
+    
+    #Modify index
+    print(my_data_frame.reset_index())
+    print("")
+    print(my_data_frame.set_index("A"))
+
+    #Rename Columns
+    print(my_data_frame.columns)
+    my_data_frame.columns = [1, 2, 3, 4, 5, 6]
+    print(my_data_frame)
 
 
 if __name__ == "__main__":
